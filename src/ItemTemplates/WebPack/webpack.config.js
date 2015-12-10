b061014860c9f5da2af6e7949218f2d26e140637
Mads Kristensen <post@madskristensen.net>
2015-12-10 11:32:33 -0800

    Fixed item template

--- src/ItemTemplates/WebPack/webpack.config.js
+++ src/ItemTemplates/WebPack/webpack.config.js
@@ -1,15 +1,14 @@
-﻿exports.config =
-  # See http://brunch.io/ for documentation.
-  paths:
-    public: 'wwwroot'
-  files:
-    javascripts:
-      joinTo:
-        'js/app.js' : /^app/
-        'js/libs.js': /(vendor|bower_components)/
-    stylesheets:
-      joinTo:
-        '/css/app.css' : /^app/
-        '/css/libs.css' : /(vendor|bower_components)/
-  modules:
-    wrapper: false
+﻿module.exports = {
+    entry: "./src/file.js",
+    output: {
+        filename: "./dist/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'jsx-loader'
+            },
+        ]
+    }
+}
